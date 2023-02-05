d576c257ec48e1a7c55ef42203880b685b69522b
AlphanetEX <24623630+alphanetEX@users.noreply.github.com>
2023-02-04 23:03:58 -0300

    docker compose install for aarch64

---
+++ FutureIascode/dockerConfig.sh
@@ -0,0 +1,4 @@
+
+#arm64
+curl -SL https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-linux-aarch64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
