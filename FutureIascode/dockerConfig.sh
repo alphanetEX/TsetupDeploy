00bb20c20e3185a4641382446244ba5dcd0b0bdc
AlphanetEX <24623630+alphanetEX@users.noreply.github.com>
2023-02-05 22:10:19 -0300

    fix installation of docker compose in aarch64 jetson series

--- FutureIascode/dockerConfig.sh
+++ FutureIascode/dockerConfig.sh
@@ -1,4 +1,7 @@
 
-#arm64
-curl -SL https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-linux-aarch64 -o ~/.docker/cli-plugins/docker-compose
+#arm64 installing docker compose //
+
+mkdir -p ~/.docker/cli-plugins/
+curl -SL "https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-$(uname -s)-$(uname -m)" -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
+
