c08b04ad06aea3a004b11572ec854e8dcee4dae2
AlphanetEX <24623630+alphanetEX@users.noreply.github.com>
2023-02-12 11:21:36 -0300

    base for autodevops for dockercompose

--- FutureIascode/dockerConfig.sh
+++ FutureIascode/dockerConfig.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
+RELEASE_URL=https://api.github.com/repos/docker/compose-cli/releases/latest
+DOWNLOAD_URL=${DOWNLOAD_URL:-$(curl -s ${RELEASE_URL} | grep "browser_download_url.*docker-linux-arm64" | cut -d : -f 2,3)}
 
 #arm64 installing docker compose //
-
 mkdir -p ~/.docker/cli-plugins/
 curl -SL "https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-$(uname -s)-$(uname -m)" -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
-
